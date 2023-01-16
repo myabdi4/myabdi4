3a88fe878a6d8691d42db56e4eb9295f4d687ef4
Abdirahman Nur <63276170+myabdi4@users.noreply.github.com>
2023-01-16 19:58:49 +0300

    css changes

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ const logo = document.querySelector(".logo");
 const mainContainer = document.querySelector(".mainContainer");
 const welcome = document.querySelector(".welcome");
 const warning = document.querySelector(".warning");
-const description = document.querySelector(".description");
+const description = document.querySelector(".imgdescription");
 const aboutme = document.querySelector(".aboutme");
-description.addEventListener("contextmenu", (event) => event.preventDefault());
+imgdescription.addEventListener("contextmenu", (event) => event.preventDefault());
 let clickable = true;
 logo.addEventListener("click", () => {
     if (clickable) {
         welcome.classList.toggle("hidden");
         warning.classList.toggle("hidden");
-        description.classList.toggle("show");
+        imgdescription.classList.toggle("show");
         aboutme.classList.toggle("show");
     }
 });
