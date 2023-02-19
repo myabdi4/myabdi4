7a0d1eb7454e0aed53264aadf3d8fd27944ad41c
Abdirahman Nur <63276170+myabdi4@users.noreply.github.com>
2023-02-19 08:23:55 -0600

    Text-to-Speech
    Added a text to speech file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const logo = document.querySelector(".logo");
 const mainContainer = document.querySelector(".mainContainer");
 const welcome = document.querySelector(".welcome");
-const warning = document.querySelector(".warning");
+// const warning = document.querySelector(".warning");
 const description = document.querySelector(".description");
 const aboutme = document.querySelector(".aboutme");
 description.addEventListener("contextmenu", (event) => event.preventDefault());
@@ -9,7 +9,7 @@ let clickable = true;
 logo.addEventListener("click", () => {
     if (clickable) {
         welcome.classList.toggle("hidden");
-        warning.classList.toggle("hidden");
+        // warning.classList.toggle("hidden");
         description.classList.toggle("show");
         aboutme.classList.toggle("show");
     }
