06a88701def5d7860ea5a479e3da7649e00d7427
Abdirahman Nur <63276170+myabdi4@users.noreply.github.com>
2023-02-20 23:45:17 -0600

    added changes
    - Removed the extra HTML and CSS page
    - Added things together, fine tuned the site

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const logo = document.querySelector(".logo");
 const mainContainer = document.querySelector(".mainContainer");
-const welcome = document.querySelector(".welcome");
+const texttospeech = document.querySelector(".texttospeech");
 // const warning = document.querySelector(".warning");
 const description = document.querySelector(".description");
 const aboutme = document.querySelector(".aboutme");
@@ -8,7 +8,7 @@ description.addEventListener("contextmenu", (event) => event.preventDefault());
 let clickable = true;
 logo.addEventListener("click", () => {
     if (clickable) {
-        welcome.classList.toggle("hidden");
+        texttospeech.classList.toggle("hidden");
         // warning.classList.toggle("hidden");
         description.classList.toggle("show");
         aboutme.classList.toggle("show");
