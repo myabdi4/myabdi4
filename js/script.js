a8cc1488415bc5997992496b55a567a0d38c75a3
Abdirahman Nur <63276170+myabdi4@users.noreply.github.com>
2023-02-21 00:55:03 -0600

    Added Changes.
    - added Hide effed to the project 2 div

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const logo = document.querySelector(".logo");
 const mainContainer = document.querySelector(".mainContainer");
 const texttospeech = document.querySelector(".texttospeech");
-// const warning = document.querySelector(".warning");
+const projectTwo = document.querySelector(".projectTwo");
 const description = document.querySelector(".description");
 const aboutme = document.querySelector(".aboutme");
 description.addEventListener("contextmenu", (event) => event.preventDefault());
 let clickable = true;
+ 
 logo.addEventListener("click", () => {
     if (clickable) {
         texttospeech.classList.toggle("hidden");
-        // warning.classList.toggle("hidden");
+        projectTwo.classList.toggle("hidden");
         description.classList.toggle("show");
         aboutme.classList.toggle("show");
     }
